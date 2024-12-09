e761c87975a302b826d54ea91062d99c8cc7e84b
anthony <ac.nicholls@gmail.com>
2024-12-08 21:56:29 -0500

    add dotnet framework project

---
+++ workflow-tests-ui-framework/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace workflow_tests_ui_framework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
